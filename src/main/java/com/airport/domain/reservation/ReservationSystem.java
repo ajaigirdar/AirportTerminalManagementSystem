8ffa1f1db38f2857133aeab782c692f5ce41f910
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-01-31 15:17:57 -0500

    Fix #3: Completed Reservation System

--- src/main/java/com/airport/domain/reservation/ReservationSystem.java
+++ src/main/java/com/airport/domain/reservation/ReservationSystem.java
@@ -1,4 +1,31 @@
 package com.airport.domain.reservation;
 
+import com.airport.domain.model.Passenger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ReservationSystem {
+
+    private HashMap<String, ArrayList<Passenger>> reservations;
+
+    public ReservationSystem() {
+        reservations = new HashMap<>();
+    }
+
+    public void addReservation(String flightNumber, Passenger passenger) {
+        if(!reservations.containsKey(flightNumber)) {
+            reservations.put(flightNumber, new ArrayList<>());
+        }
+        reservations.get(flightNumber).add(passenger);
+    }
+
+    public List<Passenger> getPassengersForFlight(String flightNumber) {
+        if (reservations.containsKey(flightNumber)) {
+            return reservations.get(flightNumber);
+        } else {
+            return new ArrayList<>(); // instead of returning null
+        }
+    }
 }
